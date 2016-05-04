c98cfb0a927dd1f554c488bc7c6999b489502ffb
Christian Siegel <christiansiegel@users.noreply.github.com>
2016-05-04 05:19:05 -0700

    use static golomb rice parameter k

--- felics/felics/felics.c
+++ felics/felics/felics.c
@@ -1,20 +1,20 @@
-#include "felics.h"
+#include "felics.h"
+
 enum {
-	MAX_VAL = 255,	MAX_K = 8, // = ceillog_2(MAX_VAL)
+	MAX_VAL = 255,
+	K = 4,
 	IN_RANGE     = 0,
 	OUT_OF_RANGE = 1,
 	BELOW_RANGE  = 0,
 	ABOVE_RANGE  = 1
-};
+};
+
 uint32_t encode(uint8_t *img_in, uint8_t *img_out)
 {
 	set_encoded_buf_for_encoding(img_out);
 
 	// neighbor memory      // ****456789...255
 	uint8_t line[256];      // 0123P*****...255
-
-	uint32_t cumul_k[256][MAX_K];
-	memset(cumul_k, 0, sizeof(cumul_k));
 
 	uint8_t x = 0;
 	uint8_t y = 0;
@@ -113,21 +113,7 @@ uint32_t encode(uint8_t *img_in, uint8_t *img_out)
 					diff = P - H - 1;
 				}
 
-				uint8_t k = 0;
-				uint32_t min = cumul_k[delta][0];
-				for (uint8_t i = 1; i < MAX_K; i++)
-				{
-					if (cumul_k[delta][i] < min)
-					{
-						min = cumul_k[delta][i];
-						k = i;
-					}
-				}
-
-				golomb_rice_encode(diff, k);
-
-				for (uint8_t i = 0; i < MAX_K; i++)
-					cumul_k[delta][i] += (diff >> i) + 1 + i;
+				golomb_rice_encode(diff, K);
 			}
 		} while (x++ != 255);
 	} while (y++ != 255);
@@ -143,9 +129,6 @@ void decode(uint8_t *img_in, uint8_t *img_out)
 
 	// neighbor memory      // ****456789...255
 	uint8_t line[256];      // 0123P*****...255
-
-	uint32_t cumul_k[256][MAX_K];
-	memset(cumul_k, 0, sizeof(cumul_k));
 
 	uint8_t x = 0;
 	uint8_t y = 0;
@@ -218,23 +201,7 @@ void decode(uint8_t *img_in, uint8_t *img_out)
 			{
 				uint8_t flag = read_bit();
 
-				uint8_t k = 0;
-				uint32_t min = cumul_k[delta][0];
-				for (uint8_t i = 1; i < MAX_K; i++)
-				{
-					if (cumul_k[delta][i] < min)
-					{
-						min = cumul_k[delta][i];
-						k = i;
-					}
-				}
-
-				uint8_t diff = golomb_rice_decode(k);
-
-				for (uint8_t i = 0; i < MAX_K; i++)
-				{
-					cumul_k[delta][i] += (diff >> i) + 1 + i;
-				}
+				uint8_t diff = golomb_rice_decode(4);
 
 				if (flag == BELOW_RANGE)
 					P = L - diff - 1;
